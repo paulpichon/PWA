dd7ce0a8e4fd04c7851029bf37e2eabcc09f3fa1
Paul Pichon F <paul10_barca@hotmail.com>
2022-11-04 20:47:50 -0600

    Activar service worker

---
+++ sw.js
@@ -0,0 +1,15 @@
+//En lugar de WINDOW se usa SELF
+
+//cuando se instala el service worker
+//se actiav una sola vez al momento de registrar el service worker
+self.addEventListener('install', e => {
+    console.log('instalado el service worker');
+    console.log( e );
+});
+
+//activar el service worker
+self.addEventListener('activate', e => {
+    console.log("service worker activado");
+
+    console.log( e );
+});
